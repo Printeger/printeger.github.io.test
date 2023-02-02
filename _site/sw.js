5c7eede921aff3efc81f5bb8273a1f0a6a03f398
printeger <printeger@gmail.com>
2023-02-02 14:42:01 +0800

    add avater

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230202.143720';
+const cacheName = 'chirpy-20230202.144101';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
